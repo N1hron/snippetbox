ab453f60fe899b9823c9dbaa3079bce0c7a46327
N1hron <denik8008@gmail.com>
2024-12-26 02:01:00 +0300

    Managing configuration settings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	port      string
+	staticDir string
+}
+
 func main() {
-	const port = 4000
+	var cfg config
+
+	flag.StringVar(&cfg.port, "port", "8080", "Server port") // Define a new command-line flag
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse() // Parse the command-line flag
 
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Println("Starting on port", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%v", port), mux)
+	log.Println("Starting on port", cfg.port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", cfg.port), mux)
 	log.Fatal(err)
 }
