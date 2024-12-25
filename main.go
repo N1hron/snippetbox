7e169c6d15eff7b117d1f94c7aca0a77f417a48b
N1hron <denik8008@gmail.com>
2024-12-25 20:29:49 +0300

    Basics

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Test")
+	const port = 4000
+
+	mux := http.NewServeMux() // Router (or servemux in Go terminology)
+	mux.HandleFunc("/", home) // Go's servemux treats the URL pattern "/" like a catch-all
+
+	log.Println("Starting on port", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), mux) // addr should be in the format "host:port"
+	log.Fatal(err)
 }
