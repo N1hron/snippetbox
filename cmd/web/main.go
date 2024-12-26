e5d0d76b479616ba86763855451e3ac16a9b6ab6
N1hron <denik8008@gmail.com>
2024-12-27 00:25:53 +0300

    Dependency injection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,36 +8,39 @@ import (
 	"os"
 )
 
-type config struct {
-	port      string
-	staticDir string
+type application struct {
+	infoLogger  *log.Logger
+	errorLogger *log.Logger
 }
 
 func main() {
-	var cfg config
-
-	flag.StringVar(&cfg.port, "port", "8080", "Server port")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	port := flag.String("port", "8080", "Server port")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
 
 	infoLogger := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLogger := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		infoLogger:  infoLogger,
+		errorLogger: errorLogger,
+	}
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir(cfg.staticDir))
+	fileServer := http.FileServer(http.Dir(*staticDir))
 
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	server := &http.Server{
-		Addr:     fmt.Sprintf(":%v", cfg.port),
+		Addr:     fmt.Sprintf(":%v", *port),
 		ErrorLog: errorLogger,
 		Handler:  mux,
 	}
 
-	infoLogger.Println("Starting on port", cfg.port)
+	infoLogger.Println("Starting on port", *port)
 	err := server.ListenAndServe()
 	errorLogger.Fatal(err)
 }
