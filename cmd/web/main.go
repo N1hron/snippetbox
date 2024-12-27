63546abc241d8233e09f5b7e5b3b9a8f333d0ac7
N1hron <denik8008@gmail.com>
2024-12-27 21:07:08 +0300

    Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,37 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type application struct {
-	port        *string
 	staticDir   *string
 	infoLogger  *log.Logger
 	errorLogger *log.Logger
 }
 
 func main() {
+	port := flag.String("port", "8080", "Server port")
+	staticDir := flag.String("static-dir", "./ui/static", "Path to static assets")
+	dsn := flag.String("dsn", "", "PostgreSQL data source name")
+
 	app := &application{
-		port:        flag.String("port", "8080", "Server port"),
-		staticDir:   flag.String("static-dir", "./ui/static", "Path to static assets"),
+		staticDir:   staticDir,
 		infoLogger:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLogger: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
 
 	flag.Parse()
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		app.errorLogger.Fatal(err)
+	}
+	defer db.Close()
+
 	server := &http.Server{
-		Addr:     fmt.Sprintf(":%v", *app.port),
+		Addr:     fmt.Sprintf(":%v", *port),
 		ErrorLog: app.errorLogger,
 		Handler:  app.routes(),
 	}
 
-	app.infoLogger.Println("Starting on port", *app.port)
-	err := server.ListenAndServe()
+	app.infoLogger.Println("Starting on port", *port)
+	err = server.ListenAndServe()
 	app.errorLogger.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
