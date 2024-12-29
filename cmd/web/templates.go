570adbc2015687c9b80d12d510b60ec32e420472
N1hron <denik8008@gmail.com>
2024-12-29 22:37:13 +0300

    Custom template functions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -20,6 +20,14 @@ func (app *application) newTemplateData() *templateData {
 	}
 }
 
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 
@@ -29,7 +37,9 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	for _, page := range pages {
-		ts, err := template.ParseFiles("./ui/html/base.html")
+		name := filepath.Base(page)
+
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.html")
 		if err != nil {
 			return nil, err
 		}
@@ -44,7 +54,6 @@ func newTemplateCache() (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		name := filepath.Base(page)
 		cache[name] = ts
 	}
 
