1361fd6930eed3dfa58da2ed31a916cd28306608
N1hron <denik8008@gmail.com>
2025-01-24 19:30:25 +0300

    Request logging

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,7 +4,6 @@ import "net/http"
 
 func secureHeaders(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		// Any code here will execute on the way down the chain.
 		w.Header().Set("Content-Security-Policy",
 			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com")
 		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
@@ -13,7 +12,15 @@ func secureHeaders(next http.Handler) http.Handler {
 		w.Header().Set("X-XSS-Protection", "0")
 
 		next.ServeHTTP(w, r)
-		// Any code here will execute on the way back up the chain.
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		app.infoLogger.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
 	}
 
 	return http.HandlerFunc(fn)
