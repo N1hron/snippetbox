ae4547333bbe7f3011b7b93b14a721ffaf59d2ae
N1hron <denik8008@gmail.com>
2025-01-27 02:05:14 +0300

    Clean URLs and method-based routing

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,18 +3,22 @@ package main
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
+
 	fileServer := http.FileServer(http.Dir(*app.staticDir))
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 
 	stardard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-	return stardard.Then(mux)
+
+	return stardard.Then(router)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.23.2
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/julienschmidt/httprouter v1.3.0
 	github.com/justinas/alice v1.2.0
 	github.com/lib/pq v1.10.9
 )
